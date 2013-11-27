8140027ef68b941ecb60a793c9ae690fca467e9a
rosenmeier <morr@itu.dk>
2013-11-27 16:06:02 +0100

    MovieTest, PersonTest added

---
+++ C#/BDSAE2013/WebServerUnitTest/MovieDtoTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebServer;
+
+namespace WebServerUnitTest
+{   
+    [TestClass]
+    public class MovieDtoTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidEmpty()
+        {
+            var movie = new MovieDto();
+            movie.Year = "";
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidSpaceChar()
+        {
+            var movie = new MovieDto();
+                movie.Year = " ";
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidChar()
+        {
+            var movie = new MovieDto();
+                movie.Year = "qwertyuiopasdfghjklzxcvbnm";
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidSpecialChar()
+        {
+            var movie = new MovieDto();
+                movie.Year = "!#¤%&/()=?+-*^¨";
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidNegativeYear()
+        {
+            var movie = new MovieDto();
+                movie.Year = "-1989";
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidCharInFront()
+        {
+            var movie = new MovieDto();
+                movie.Year = "year2012";
+        }
+
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidFiveDigit()
+        {
+            var movie = new MovieDto();
+                movie.Year = "20012";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestYearInputInValidThreeDigit()
+        {
+            var movie = new MovieDto();
+            movie.Year = "209";
+        }
+
+        [TestMethod]
+        public void TestYearInputValid()
+        {
+            var movie = new MovieDto();
+            movie.Year = "1992";
+            movie.Year = "2014";
+            movie.Year = "0000";
+            movie.Year = "9999";
+        }
+
+
+    }
+}
---
+++ C#/BDSAE2013/WebServerUnitTest/PersonDtoTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebServer;
+
+namespace WebServerUnitTest
+{
+    [TestClass]
+    class PersonDtoTest
+    {
+    }
+}
