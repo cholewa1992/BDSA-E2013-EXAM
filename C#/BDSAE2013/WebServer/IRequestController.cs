d5f56d7c034045d2ad732beab7edf2a4c5a78503
Jonas Kastberg <jihgfee@gmail.com>
2013-11-28 15:37:16 +0100

    Added Request Delegator

---
+++ C#/BDSAE2013/WebServer/IRequestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunicationFramework;
+
+namespace WebServer
+{
+    public interface IRequestController
+    {
+        string Keyword { get; set; }
+
+        void ProcessRequest(Request request);
+
+    }
+
+
+
+}
---
+++ C#/BDSAE2013/WebServer/RequestDelegator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunicationFramework;
+
+namespace WebServer
+{
+    public class RequestDelegator
+    {
+        IRequestController[] requestControllerArray;
+        public void ProcessRequest(Request request)
+        {
+            DefineController(request.Method).ProcessRequest(request);
+        }
+
+        private IRequestController DefineController(string method)
+        {
+            string url = method.Split(' ')[1];
+            string[] urlKeywords = url.Split('/');
+
+            for (int i = 0; i < urlKeywords.Length; i++)
+            {
+                for (int j = 0; j < requestControllerArray.Length; j++)
+                {
+                    if( urlKeywords[i] == requestControllerArray[j].Keyword )
+                    {
+                        return requestControllerArray[j];
+                    }
+                }
+            }
+
+            throw new ArgumentException("The url did not match any controllers");
+        }
+
+    }
+}
