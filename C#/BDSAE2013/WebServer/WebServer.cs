c68665aa4eaf1d564dc0a1eee233bf36041f4d67
mktitan <mk_titan@hotmail.com>
2013-11-28 17:37:36 +0100

    WebServer Main initial implementation

---
+++ C#/BDSAE2013/WebServer/WebServer.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System;
+using System.Threading.Tasks;
+using CommunicationFramework;
+
+namespace WebServer
+{
+
+
+    public class WebServer
+    {
+
+        private readonly CommunicationHandler _communicationHandler;
+
+
+        public static void Main(String[] args)
+        {
+
+            new WebServer("http://localhost:1337", Protocols.HTTP).Start();
+
+        }
+
+
+        public WebServer(String Uri, Protocols protocol)
+        {
+
+            _communicationHandler = new CommunicationHandler(protocol);
+
+        }
+
+
+        public void Start()
+        {
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                Request request = _communicationHandler.GetRequest();
+                Task.Run(() => new RequestDelegator().ProcessRequest(request));
+                Console.WriteLine("new thread started");
+            }
+
+        }
+
+    }
+}
