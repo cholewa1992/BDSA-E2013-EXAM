e05de1be4f8177dac733de7257a3d4ec7c37cd72
Capsup <capsup@live.dk>
2013-12-04 16:45:16 +0100

    Added AspClient as seperate solution

---
+++ C#/AspClient/AspClient/Models/HomeModels.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace MvcApplication2.Models
+{
+    public class HomeModels
+    {
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Kind { get; set; }
+        public string Year { get; set; }
+        public int SeasonNumber { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string SeriesYear { get; set; }
+        public int EpisodeOfId { get; set; }
+
+        public Movie(string jsonString)
+        {
+            
+        }
+    }
+}
