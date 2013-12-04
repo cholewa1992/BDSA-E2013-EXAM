2d963b4a1782fdb5dcab455a47b3f44926390c3d
Jonas Kastberg <jihgfee@gmail.com>
2013-12-04 11:42:05 +0100

    Deleted Program.cs file in WebServer

--- C#/BDSAE2013/WebServer/WebServer.cs
+++ C#/BDSAE2013/WebServer/WebServer.cs
@@ -11,6 +11,75 @@ namespace WebServer
         public static void Main( String[] args )
         {
             new WebServer().Start( "http://localhost:1337/", Protocols.HTTP );
+
+            RequestDelegator delegator = new RequestDelegator();
+
+            //            Request request = new Request();
+
+            //            //Test movie processing
+            //            request.Method = "GET http://localhost:112/Movie/5";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "POST http://localhost:112/Movie";
+            //            request.Data = Encoding.GetEncoding("iso-8859-1").GetBytes(JSonParser.Parse(
+            //                "title", "Die Hard",
+            //                "kind", "Drama",
+            //                "year", "1992",
+            //                "seasonNumber", "0",
+            //                "episodeNumber", "0",
+            //                "seriesYear", "0",
+            //                "episodeOfId", "0"));
+            //            delegator.ProcessRequest(request);
+
+            //            /*
+            //            request.Method = "PUT http://localhost:112/Movie";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "DELETE http://localhost:112/Movie";
+            //            delegator.ProcessRequest(request);
+
+            //            //Test user processing
+            //            request.Method = "GET http://localhost:112/User";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "POST http://localhost:112/User";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "PUT http://localhost:112/User";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "DELETE http://localhost:112/User";
+            //            delegator.ProcessRequest(request);
+
+            //            //Test Person processing
+
+            //            request.Method = "GET http://localhost:112/Person";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "POST http://localhost:112/Person";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "PUT http://localhost:112/Person";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "DELETE http://localhost:112/Person";
+            //            delegator.ProcessRequest(request);
+
+            //            //Test Favourite processing
+            //            request.Method = "GET http://localhost:112/Favourite";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "POST http://localhost:112/Favourite";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "PUT http://localhost:112/Favourite";
+            //            delegator.ProcessRequest(request);
+
+            //            request.Method = "DELETE http://localhost:112/Favourite";
+            //            delegator.ProcessRequest(request);
+
+            //             * */
+            //            Console.ReadKey();
         }
 
         public void Start( String listenAddress, Protocols protocol )
